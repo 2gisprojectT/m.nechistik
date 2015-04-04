523f812bf2ecc926af8845d0714be29bd50a0ee4
Mikhail0404 <mikhail0404@yandex.ru>
2015-04-04 11:12:12 +0500

    Update Lion.py

--- 30.03_Lion/Lion.py
+++ 30.03_Lion/Lion.py
@@ -7,31 +7,32 @@ class Lion:
         # Initially Lion is always hungry
         self.Lion_status = Lion_status
         self.Lion_action = Lion_action
+        self.obj = ""
 
-
+    def init_obj(self, text):
+        self.obj = text
 
     # get current Lion status
     def get_status(self):
         return self.Lion_status
 
-    def set_status(self, text):
-        self.Lion_status = self.Lion_action[(text, self.Lion_status)][1]
+    def change_status(self):
+        self.Lion_status = self.Lion_action[(self.obj, self.Lion_status)][1]
 
-
-    def get_action(self, text):
-        return self.Lion_action[(text, self.Lion_status)][0]
+    def get_action(self):
+        return self.Lion_action[(self.obj, self.Lion_status)][0]
 
 
 if __name__ == '__main__':
 
     # Initialization
     Lion_action = {('антилопа', 'сытый'): ('спать, и переходит в состояние голодный', 'голодный'),
-                            ('охотник', 'сытый'): ('убежать от охотника, и переходит в состояние голодный', 'голодный'),
-                            ('дерево', 'сытый'): ('смотреть на дерево, и переходит в состояние голодный', 'голодный'),
+                    ('охотник', 'сытый'): ('убежать от охотника, и переходит в состояние голодный', 'голодный'),
+                    ('дерево', 'сытый'): ('смотреть на дерево, и переходит в состояние голодный', 'голодный'),
 
-                            ('антилопа', 'голодный'): ('съесть антилопу, и переходит в состояние сытый', 'сытый'),
-                            ('охотник', 'голодный'): ('убежать от охотника, и остаётся голодным', 'голодный'),
-                            ('дерево', 'голодный'): ('спать в тени дерева, и остаётся голодным', 'голодный')}
+                    ('антилопа', 'голодный'): ('съесть антилопу, и переходит в состояние сытый', 'сытый'),
+                    ('охотник', 'голодный'): ('убежать от охотника, и остаётся голодным', 'голодный'),
+                    ('дерево', 'голодный'): ('спать в тени дерева, и остаётся голодным', 'голодный')}
 
     Lion_status = 'голодный'
     L = Lion(Lion_action, Lion_status)
@@ -50,8 +51,9 @@ if __name__ == '__main__':
         else:
             # For wrong data
             try:
-                print('Лев решает ' + L.get_action(text))
-                L.set_status(text)
+                L.init_obj(text)
+                print('Лев решает ' + L.get_action())
+                L.change_status()
             except:
                 print('Ошибка: Лев не знает что делать!')
 
