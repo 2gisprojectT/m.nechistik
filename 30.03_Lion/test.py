7edacf8bb9cad9ecef938c93044850a25a9a80b0
Mikhail0404 <mikhail0404@yandex.ru>
2015-04-06 22:14:14 +0500

    Update test.py

--- 30.03_Lion/test.py
+++ 30.03_Lion/test.py
@@ -4,85 +4,53 @@ from unittest import TestCase
 from Lion import Lion
 import unittest
 
-# Test for class Lion
-class LionTest(TestCase):
-    Lion_script = {('антилопа', 'сытый'): ('спать, и переходит в состояние голодный', 'голодный'),
-                    ('охотник', 'сытый'): ('убежать от охотника, и переходит в состояние голодный', 'голодный'),
-                    ('дерево', 'сытый'): ('смотреть на дерево, и переходит в состояние голодный', 'голодный'),
 
-                    ('антилопа', 'голодный'): ('съесть антилопу, и переходит в состояние сытый', 'сытый'),
-                    ('охотник', 'голодный'): ('убежать от охотника, и остаётся голодным', 'голодный'),
-                    ('дерево', 'голодный'): ('спать в тени дерева, и остаётся голодным', 'голодный')}
+class LionTest(TestCase):
+    Lion_script = {('антилопа', 'голодный'): ('съесть', 'сытый')}
 
     def test_init(self):
         Lion_status = 'голодный'
         L = Lion(self.Lion_script, Lion_status)
+        self.assertEqual('голодный', L.Lion_status, 'Error, wrong status')
+        self.assertEqual('', L.Lion_action, 'Error, wrong action')
+        self.assertEqual(self.Lion_script, L.Lion_script, 'Error, wrong dict')
 
-        # set status and check that it is possible to get the correct current status
-        self.assertEqual('голодный', L.Lion_status, 'Error when status hungry in Lion')
-        self.assertEqual('', L.Lion_action, 'Error when status hungry in Lion')
-        self.assertEqual(self.Lion_script, L.Lion_script, 'Error when status hungry in Lion')
-
-
-        Lion_status = 'сытый'
+    def test_init_empty_lion_status(self):
+        Lion_status = ''
         L = Lion(self.Lion_script, Lion_status)
+        self.assertIsNone(L.Lion_status, 'Error')
 
-        # set status and check that it is possible to get the correct current status
-        self.assertEqual('сытый', L.Lion_status, 'Error when status hungry in Lion')
-        self.assertEqual('', L.Lion_action, 'Error when status hungry in Lion')
-        self.assertEqual(self.Lion_script, L.Lion_script, 'Error when status hungry in Lion')
+    def test_init_wrong_lion_status(self):
+        Lion_status = 'nxfd'
+        L = Lion(self.Lion_script, Lion_status)
+        self.assertIsNone(L.Lion_status, 'Error')
 
-
-
-    def test_do_all_antelope(self):
+    def test_do_all(self):
         Lion_status = 'голодный'
         L = Lion(self.Lion_script, Lion_status)
-
         L.do_all('антилопа')
-        self.assertEqual('сытый', L.Lion_status, 'Error when status hungry in Lion')
-        self.assertEqual('съесть антилопу, и переходит в состояние сытый', L.Lion_action, 'Error when status hungry in Lion')
+        self.assertEqual('сытый', L.Lion_status, 'Error, wrong status')
+        self.assertEqual('съесть', L.Lion_action, 'Error, wrong action')
 
-
-        Lion_status = 'сытый'
-        L = Lion(self.Lion_script, Lion_status)
-
-        L.do_all('антилопа')
-        self.assertEqual('голодный', L.Lion_status, 'Error when status hungry in Lion')
-        self.assertEqual('спать, и переходит в состояние голодный', L.Lion_action, 'Error when status hungry in Lion')
-
-
-    def test_do_all_hunter(self):
+    def test_do_all_wrong_data(self):
         Lion_status = 'голодный'
         L = Lion(self.Lion_script, Lion_status)
+        try:
+            L.do_all('dshsdhsfh')
+        except:
+            print('')
+        self.assertEqual('голодный', L.Lion_status, 'Error, wrong status, when wrong data')
+        self.assertEqual('', L.Lion_action, 'Error, wrong action, when wrong data')
 
-        L.do_all('охотник')
-        self.assertEqual('голодный', L.Lion_status, 'Error when status hungry in Lion')
-        self.assertEqual('убежать от охотника, и остаётся голодным', L.Lion_action, 'Error when status hungry in Lion')
-
-
-        Lion_status = 'сытый'
-        L = Lion(self.Lion_script, Lion_status)
-
-        L.do_all('охотник')
-        self.assertEqual('голодный', L.Lion_status, 'Error when status hungry in Lion')
-        self.assertEqual('убежать от охотника, и переходит в состояние голодный', L.Lion_action, 'Error when status hungry in Lion')
-
-
-    def test_do_all_tree(self):
+    def test_do_all_empty_data(self):
         Lion_status = 'голодный'
         L = Lion(self.Lion_script, Lion_status)
-
-        L.do_all('дерево')
-        self.assertEqual('голодный', L.Lion_status, 'Error when status hungry in Lion')
-        self.assertEqual('спать в тени дерева, и остаётся голодным', L.Lion_action, 'Error when status hungry in Lion')
-
-
-        Lion_status = 'сытый'
-        L = Lion(self.Lion_script, Lion_status)
-
-        L.do_all('дерево')
-        self.assertEqual('голодный', L.Lion_status, 'Error when status hungry in Lion')
-        self.assertEqual('смотреть на дерево, и переходит в состояние голодный', L.Lion_action, 'Error when status hungry in Lion')
+        try:
+            L.do_all('')
+        except:
+            print('')
+        self.assertEqual('голодный', L.Lion_status, 'Error, wrong status, when wrong data')
+        self.assertEqual('', L.Lion_action, 'Error, wrong action, when wrong data')
 
 if __name__ == '__main__':
     unittest.main()
